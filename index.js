1c59d4c695aefa0b584a52c04552ca897ac86d9c
Dexedus <karlfleming64@gmail.com>
2024-12-04 13:41:18 +0000

    Add automatic custom emailing to customer once stripe payment is successful

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const app = express();
 const router = express.Router();
+const sgMail = require("@sendgrid/mail")
+const nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,8 +14,84 @@ const port = 3000
 //Middleware
 app.use(express.json());
 app.use(express.static("public"));
+app.use(bodyParser.raw({ type: 'application/json' }));
+
+const endpointSecret = process.env.WEBHOOK_SECRET
+const API_KEY = process.env.SEND_GRID_KEY
+const SendgridSender = process.env.EMAIL
+
+sgMail.setApiKey(`${API_KEY}`)
 
 
+// SendGrid email template
+const sendEmail = (toEmail, subject, message) => {
+  const msg = {
+    to: toEmail,
+    from: `${SendgridSender}`, // Verified SendGrid sender email
+    subject: subject,
+    text: message,
+  };
+
+  // Send the email via SendGrid
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent successfully');
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+};
+
+
+
+
+
+
+// Stripe webhook handler
+app.post('/stripe/webhook', (req, res) => {
+  const sig = req.headers[`${endpointSecret}`];
+
+  let event;
+
+  // Verify the Stripe webhook signature
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+  } catch (err) {
+    console.error('Error verifying webhook signature:', err);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle successful payment (for both Stripe Checkout and Payment Intents)
+  if (event.type === 'payment_intent.succeeded' || event.type === 'checkout.session.completed') {
+    const paymentIntent = event.data.object;
+    console.log(paymentIntent)
+    const customerEmail = paymentIntent.receipt_email || paymentIntent.customer_email; // Get the customer email
+
+    // Define your custom email content
+    const subject = 'Thank you for your purchase!';
+    const message = `Dear customer,
+
+Thank you for your purchase. Your payment has been successfully processed.
+This test email is working!
+
+Best regards,
+Max Grind School`;
+
+    // Send the custom email via SendGrid
+    sendEmail(customerEmail, subject, message);
+  }
+
+  // Acknowledge receipt of the event
+  res.json({ received: true });
+});
+
+
+
+
+
+
+//routes
 app.get('/', (req, res) => {
     res.render("home.ejs")
 })
