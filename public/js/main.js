44c19ed10e339071741d18184b6e7c11647df874
Dexedus <karlfleming64@gmail.com>
2024-12-18 01:39:01 +0000

    fix modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 const modal = document.getElementById("popup");
 const closeBtn = document.getElementById("close");
+const quit = document.getElementById("quit");
 
 closeBtn.onclick = function() {
+    modal.classList.add("closingAnimation")
+}
+
+quit.onclick = function() {
     modal.classList.add("closingAnimation")
 }
