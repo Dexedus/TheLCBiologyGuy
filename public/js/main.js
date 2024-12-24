87db51cd25796fa8e3399af7e218e16eabecafd0
Dexedus <karlfleming64@gmail.com>
2024-12-24 15:22:32 +0000

    alter price cards

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,11 @@
 const modal = document.getElementById("popup");
 const closeBtn = document.getElementById("close");
-const quit = document.getElementById("quit");
+const quitBtn = document.getElementById("quit");
 
 closeBtn.onclick = function() {
     modal.classList.add("closingAnimation")
 }
 
-quit.onclick = function() {
+quitBtn.onclick = function() {
     modal.classList.add("closingAnimation")
 }
-
-
-// JavaScript to show the video after the user clicks 'Play'
-document.getElementById('playButton').addEventListener('click', function() {
-    // Hide the play button
-    document.getElementById('playButton').style.display = 'none';
-
-    // Show the TikTok video embed
-    var iframe = document.getElementById('tiktokVideo');
-    iframe.style.display = 'block';
-
-    // Trigger autoplay by reloading the iframe with autoplay set to 1
-    iframe.src = iframe.src.replace("autoplay=0", "autoplay=1");
-});
