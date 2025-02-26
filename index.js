6f4fb882e4db0edf53b0c0d24df834ee04c39293
Dexedus <karlfleming64@gmail.com>
2025-02-26 14:39:39 +0000

    fix

--- index.js
+++ index.js
@@ -54,8 +54,6 @@ app.post('/stripe/webhook', bodyParser.raw({ type: 'application/json' }), async 
 
       console.log(products)
 
-
-      
     
     }
   }
@@ -395,7 +393,7 @@ app.get('/contact', (req, res) => {
 })
 
 app.get("/done", (req, res) => {
-  res.render("landing.ejs", {
+  res.render("success.ejs", {
     title: "Success!",
     message: "You should recieve the details for these resources via an email sent to the address you entered at checkout. Be sure to check your spam just in case it gets filtered there. If you do not recieve an email after 24 hours please contact me at: thelcbiologyguy@gmail.com Thanks.",
     button: "Close",
