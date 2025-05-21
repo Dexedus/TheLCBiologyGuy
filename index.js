2224e9ea544576a3f633dca780d25403b3d47439
Dexedus <karlfleming64@gmail.com>
2025-05-21 23:04:56 +0100

    done

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/stripe/webhook', bodyParser.raw({ type: 'application/json' }), async 
 
 
 
-        if(products[0] === 'Unit 1 and The Cell Masterclasses' || products[0] === 'Cell & Ecology Free Masterclasses (March 6th and 9th)' || products[0] === 'Improvement Bundle' ){
+        if(products[0] === 'Unit 1 and The Cell Masterclasses' || products[0] === 'Cell & Ecology Free Masterclasses (March 6th and 9th)' || products[0] === 'Improvement Bundle' || products[0] === 'Final Class Product' ){
 
           let message = ""
           let subject = ""
@@ -177,6 +177,16 @@ app.post('/stripe/webhook', bodyParser.raw({ type: 'application/json' }), async 
             <button type="submit">Submit</button>
           </form>`;
           productTable = 'free_resources_emails';
+        } else if (products[0] === 'Final Class Product') {
+            subject = 'Thank you for choosing Final Class Product!';
+            message = `Dear ${firstName},<br><br>Thank you for choosing the final class. Here is the link to the Google Drive containing the resources: <a href="https://u48917275.ct.sendgrid.net/ls/click?upn=u001.gb1oIQZYL4vnMZkgmvEgigzFl42rVVPLGu-2Fe519Dvun9tuRbO-2FbM7IplLEtFJNpQ05TKwRq03odmolpArth0ldjiurLFB4dCM-2B4tixT-2F0TJ1ELxqIhhbS32gO3hKFnrEIFcd_4pE3C559McDKAd-2Fg3v7vn7eIndNn6ci9X9Lg05SN5hd0HqQd0CGpTiKRONJude4-2BSsNEXmpTWFbVn7KIYUZRVHAyrUpW7MXxjc-2FqCDWugVFXx574jVw6J7AuqIMN8xCK0iv3bPZjXrabb-2BWXwezZpQFLZE34yn6CVbJCQvmrQ3rjg5a43SNZwK-2BgAipFyVeR3EkkRmw-2B21-2FGCOBGcKlZTw-3D-3D" target="_blank">Here</a><br><br>We would like to send you promotional emails from time to time. But if you don't want us to, that's okay. Just tick the box below, and submit so we can exclude you from our promotions list.<br><br>Best Regards,<br>Max<br>
+            <form action="https://www.thelcbiologyguy.ie/unsubscribe" method="POST">
+              <input type="checkbox" name="unsubscribe">
+              <label for="unsubscribe">I wish to opt out of future promotional emails from The LC Biology Guy</label><br>
+              <input type="hidden" name="email" value="${customerEmail}">
+              <button type="submit">Submit</button>
+            </form>`;
+            productTable = 'finalclass';
         } else if (products[0] === 'Improvement Bundle') {
           subject = 'H1 Fast-Track Bundle';
           message = `Dear ${firstName},<br><br>Welcome to the 8-week Biology H1 Fast-Track Program! You can join the live Zoom session using the link(s) below.<br><br>
